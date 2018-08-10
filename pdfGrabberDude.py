4c3c68050d2db9885ab93baad2812f51513f47c1
bassbreaker <bass_breaker@hotmail.ca>
2018-08-10 17:52:21 -0400

    init

---
+++ pdfGrabberDude.py
@@ -0,0 +1,10 @@
+import textract
+
+BIBLE_DICT = {'Genesis':1,'Exodus':2,'Leviticus':3,'Numbers':4,'Deuteronomy':5,'Joshua':6,'Judges':7,'Ruth':8,'Samuel':'check9',
+              'Kings':'check11','Chronicles':'check13','Ezra':15,'Nehemiah':16,'Esther':17,'Job':18,'Psalms':19,'Proverbs':20,
+              'Ecclesiastes':21,'Solomon':22,'Isaiah':23,'Jeremiah':24,'Lamentations':25,'Ezekiel':26,'Daniel':27,
+              'Hosea':28,'Joel':29,'Amos':30,'Obadiah':31,'Jonah':32,'Micah':33,'Nahum':34,'Habakkuk':35,'Zephaniah':36,
+              'Haggai':37,'Zechariah':38,'Malachi':39,'Matthew':40,'Mark':41,'Luke':42,'John':'check43','Acts':44,'Romans':45,
+              'Corinthians':'check46','Galatians':48,'Ephesians':49,'Philippians':50,'Colossians':51,'Thessalonians':'check52',
+              'Timothy':'check54','Titus':56,'Philemon':57,'Hebrews':58,'James':59,'Peter':'check60','Jude':65,'Revelation':66}
+              
